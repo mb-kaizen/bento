c650ed8822c9ac2768ddf8927db3cd1ab43a40a7
Michael Brown <michael.entr@gmail.com>
2018-03-26 20:38:08 -0400

    added devise gem and set up user authentication

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'bootstrap', '4.0.0.alpha6'
 gem 'will_paginate', '~> 3.1.0'
 gem 'simple_form'
 gem 'font-awesome-rails'
+gem 'devise'
 
 
 source 'https://rails-assets.org' do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'bento-michael-brown.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
